6cc60a8413102041ad37b596ad86aa8576eb1fa3
HeesungB <32606183+HeesungB@users.noreply.github.com>
2022-03-04 10:36:31 +0900

    test: original source

--- index.js
+++ index.js
@@ -3,27 +3,26 @@ const path = require('path');
 const { promises: fs } = require('fs');
 
 const main = async () => {
-  console.log('main');
-  // const readPath = core.getInput('read-path');
-  // console.log('readPath', readPath);
-  // const writePath = core.getInput('write-path');
-  // console.log('writePath', writePath);
-  // const fullPath = path.join(process.env.GITHUB_WORKSPACE, writePath);
-  // console.log('fullPath', fullPath);
+  const readPath = core.getInput('read-path');
+  console.log('readPath', readPath);
+  const writePath = core.getInput('write-path');
+  console.log('writePath', writePath);
+  const fullPath = path.join(process.env.GITHUB_WORKSPACE, writePath);
+  console.log('fullPath', fullPath);
 
-  // const merge = {};
+  const merge = {};
 
-  // const files = await fs.readdir(readPath, 'utf8');
+  const files = await fs.readdir(readPath, 'utf8');
 
-  // for (const file of files) {
-  //   const jsonFile = await fs.readFile(`${readPath}/${file}`, 'utf8');
-  //   const json = JSON.parse(jsonFile.toString());
-  //   console.log(file, json);
+  for (const file of files) {
+    const jsonFile = await fs.readFile(`${readPath}/${file}`, 'utf8');
+    const json = JSON.parse(jsonFile.toString());
+    console.log(file, json);
 
-  //   Object.assign(merge, json);
-  // }
+    Object.assign(merge, json);
+  }
 
-  // await fs.writeFile(fullPath, JSON.stringify(merge));
+  await fs.writeFile(fullPath, JSON.stringify(merge));
 };
 
 main().catch((err) => core.setFailed(err.message));
